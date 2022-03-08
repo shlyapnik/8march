113afcdabb9f3156331b2bccc432dd2d361bad81
shlyapnik <77507745+shlyapnik@users.noreply.github.com>
2022-03-09 01:16:31 +0500

    parallax

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener("mousemove", parallax);
 
 function parallax(e) {
-    this.querySelectorAll(".parallax").forEach(img => {
-        const speed = img.getAttribute("data-speed");
+    this.querySelectorAll(".parallax").forEach(parallax => {
+        const speed = parallax.getAttribute("data-speed");
 
         const x = (window.innerWidth - e.pageX * speed) / 100;
         const y = (window.innerHeight - e.pageY * speed) / 100;
 
-        img.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        parallax.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
 }
